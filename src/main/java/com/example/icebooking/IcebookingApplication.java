bf65e947acf6b121a9dbe712d67fcbefbd7ae626
ngdream <96875506+ngdream@users.noreply.github.com>
2023-10-23 12:34:57 +0100

    my commit

---
+++ src/main/java/com/example/icebooking/IcebookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.icebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcebookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcebookingApplication.class, args);
+	}
+
+}
